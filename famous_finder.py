31ac3c4fdb0f2acc2af1c78332a97507cbed1763
arielnasimba <arielkafe@hotmail.com>
2025-07-20 18:42:13 +0200

    feat(famous_people_asking) : update connect_execute_to_db feat, create insert_to_db, famous_people_asking and main program

--- famous_finder.py
+++ famous_finder.py
@@ -1,20 +1,62 @@
 import wikipedia
 import sqlite3
 
-def connect_execute_to_db(sql_req):
+def connect_execute_to_db(sql_req, params=()):
+    # connect to database
     con = sqlite3.connect("test_snel.db")
     cur = con.cursor()
-    cur.execute(sql_req)
+    
+    # execute sql request
+    cur.execute(sql_req, params)
     con.commit()
+    print(cur.execute("SELECT COUNT(*) FROM famous_people;").fetchall())
+    print(cur.execute("SELECT ID as 'ID', name 'Name' FROM famous_people;").fetchall())
     con.close()
 
 def create_db():
-    con = sqlite3.connect("test_snel.db")
-    cur = con.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS famous_people(
+    # create database
+    sql_req = '''CREATE TABLE IF NOT EXISTS famous_people(
                         ID INTEGER PRIMARY KEY AUTOINCREMENT,
                         name VARCHAR(255),
-                        summary TEXT);''')
-    con.commit()
-    con.close()
+                        summary TEXT);'''
+    connect_execute_to_db(sql_req)
     
+def insert_to_db(famous_name, famous_summary):
+    # insert into database
+    sql_req = "INSERT INTO famous_people(name, summary) VALUES (?, ?);"
+    connect_execute_to_db(sql_req, (famous_name, famous_summary))
+
+def famous_people_asking():
+    name = input("Can you give me the name of a famous person?\n>>> ").strip()
+
+    try:
+        # Check search results
+        search_results = wikipedia.search(name)
+        if not search_results:
+            print("Sorry there are no results for that.")
+            return
+        #Get first result
+        first_match = search_results[0]
+
+        if first_match.lower() != name.lower():
+            print("I don’t know this person.")
+            if wikipedia.suggest(name):
+                print(f"Did you mean: {wikipedia.suggest(name)}?")
+            else:
+                print("No suggestion found.")
+            return
+
+        # If exact match, continue
+        summary = wikipedia.summary(name)
+        insert_to_db(name, summary)
+
+    except wikipedia.exceptions.DisambiguationError as e:
+        print("Multiple results found. Try one of these:")
+        print(e.options[:3])
+    except wikipedia.exceptions.PageError:
+        print("I don’t know this person.")
+        
+        
+# Run program
+create_db()
+famous_people_asking()
